f41d04b13d9b599605261b1d73d6f9ec39234966
longnc <nclong92@gmail.com>
2020-03-12 10:07:33 +0700

    Add project files.

---
+++ DesktopToastsApp/NewToastNotification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace DesktopToastsApp
+{
+    public class NewToastNotification
+    {
+        public NewToastNotification(string title, string content, int type)
+        {
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            XmlNodeList toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode($"{content}"));
+
+            XmlNodeList toastImageAttributes = toastXml.GetElementsByTagName("image");
+            ((XmlElement)toastImageAttributes[0]).SetAttribute("src", "C:\\Users\\IsaoTakashi\\Desktop\\desktop-toasts-master\\CS\\DesktopToastsApp\\Images\\883-364x202.jpg");
+            ((XmlElement)toastImageAttributes[0]).SetAttribute("alt", "red graphic");
+
+            IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            ((XmlElement)toastNode).SetAttribute("duration", "long");
+
+            ((XmlElement)toastNode).SetAttribute("launch", "{\"type\":\"toast\",\"param1\":\"12345\",\"param2\":\"67890\"}");
+            ToastNotification toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier($"{title}").Show(toast);
+        }
+    }
+}
---
+++ DesktopToastsApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopToastsApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            NewToastNotification Window = new NewToastNotification("VTC RD Notification", "This is notify from VTC ITS Center. Thank you for watching.", 3);
+
+            await Task.Delay(5000);
+        }
+    }
+}
